d7ff67e8705fbf5e7f490b52692ea64bad28ace4
Co-vengers <vyomrohila@gmail.com>
2025-03-25 22:23:49 +0530

    settings updated

--- emergency_qr/emergency_qr/settings.py
+++ emergency_qr/emergency_qr/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-p*p@p$)^b!vh(%9cw%6m&87^s$k!sbdrxty-zc#0gb#kou57v-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
