2ceeaca86bd541a08b3be5d4fd77edaa8fac694d
Co-vengers <vyomrohila@gmail.com>
2025-03-27 18:24:47 +0530

    settings updated

--- emergency_qr/emergency_qr/settings.py
+++ emergency_qr/emergency_qr/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-p*p@p$)^b!vh(%9cw%6m&87^s$k!sbdrxty-zc#0gb#kou57v-"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -48,10 +49,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'vyomrohila@gmail.com'
-EMAIL_HOST_PASSWORD = 'owppicoktihlciys'  # Use environment variables for security
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')  # Use environment variables for security
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')  # Use environment variables for security
 
-DEFAULT_FROM_EMAIL = 'vyomrohila@gmail.com'
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_HOST_USER')  # Use environment variables for security
 
 
 
